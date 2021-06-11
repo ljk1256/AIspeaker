eaeba4995880da3ee421996b1b02a3c58d6a0547
Kwoun Ki Ho <fingercut@naver.com>
2021-06-12 02:35:29 +0900

    JSON Parsing

--- eatingtoday/src/main/java/com/eatingtoday/eatingtoday/apiController.java
+++ eatingtoday/src/main/java/com/eatingtoday/eatingtoday/apiController.java
@@ -1,5 +1,6 @@
 package com.eatingtoday.eatingtoday;
 
+import lombok.extern.java.Log;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
@@ -24,7 +25,7 @@ public class apiController extends naverApicontroller{
         String clientId = "VhbUCLMP0G9GktY1da2R"; //애플리케이션 클라이언트 아이디값"
         String clientSecret = "aPV80eOIok"; //애플리케이션 클라이언트 시크릿값"
 
-
+        localdata = "강남 맛집";        //이거 지워야됨
         String text = localdata;
         String sort = "comment";
         int display = 5;
@@ -43,15 +44,37 @@ public class apiController extends naverApicontroller{
         requestHeaders.put("X-Naver-Client-Secret", clientSecret);
         String responseBody = get(apiURL,requestHeaders);
 
-        try{
+/*        try{
             JSONParser jsonParser = new JSONParser();
 
             JSONObject jsonObject = (JSONObject) jsonParser.parse(responseBody);
             JSONArray locationObject = (JSONArray) jsonObject.get("items");
 
-            for(int i=0; i<locationObject.size(); i++){
-
+            for(int i = 0; i <locationObject.size(); ++i){
+                JSONObject jsonObjects = locationObject.get(i);
             }
+
+        }*/
+        String [] info = {};
+        try{
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(responseBody.toString());
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray getArray = (JSONArray) jsonObject.get("items");
+
+            for(int i = 0; i < getArray.size(); ++i){
+                JSONObject object = (JSONObject) getArray.get(i);
+
+                String getTitle = (String) object.get("title");
+                String address = (String) object.get("address");
+                String roadAddress = (String) object.get("roadAddress");
+
+                String titleFilter = getTitle.replaceAll("<b>", "");
+                String title = titleFilter.replaceAll("</b>", "");
+
+                System.out.println("상호명: " + title + ", " + "주소: " + address + ", " + "도로명주소: " + roadAddress);
+            }
+
         }
         catch (org.json.simple.parser.ParseException e){
             e.printStackTrace();
